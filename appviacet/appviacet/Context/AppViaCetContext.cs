aab49177cbdb531f7d6e5fc1e39a8fd2d7de4b4c
Denis <denis.0001@hotmail.com>
2023-09-21 23:00:17 -0300

    add aplicacao CEP

---
+++ appviacet/appviacet/Context/AppViaCetContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace appviacet.Context
+{
+    public class AppViaCetContext : DbContext
+    {
+        public AppViaCetContext(DbContextOptions<DbContext> options) : base (options) 
+        {
+        }
+    }
+}
